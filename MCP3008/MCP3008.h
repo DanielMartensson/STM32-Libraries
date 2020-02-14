293d2cd026e4ecfc3b30a7ab19bc0a0346bd4706
Daniel Mårtensson <daniel.martensson100@outlook.com>
2020-02-14 13:35:47 +0100

    Update MCP3008.h

--- MCP3008/MCP3008.h
+++ MCP3008/MCP3008.h
@@ -12,11 +12,11 @@
 
 typedef struct {
   GPIO_TypeDef* CS_PORT;
-  uint16_t CS_PIN; 
-  SPI_HandleTypeDef hspi;
+  uint16_t CS_PIN;
+  SPI_HandleTypeDef* hspi;
 }MCP3008_SPI;
 
-void MCP3008_Init(MCP3008_SPI* spi, SPI_HandleTypeDef hspi, GPIO_TypeDef* CS_PORT, uint16_t CS_PIN);
+void MCP3008_Init(MCP3008_SPI* spi, SPI_HandleTypeDef* hspi, GPIO_TypeDef* CS_PORT, uint16_t CS_PIN);
 uint16_t MCP3008_Read_Channel(MCP3008_SPI* spi, uint8_t channel);
 
 #endif /* MCP3008_MCP3008_H_ */
