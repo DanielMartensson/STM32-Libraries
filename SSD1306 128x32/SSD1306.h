01965c4714382a35a1b0e48c0007f49f89fa43d5
Daniel Mårtensson <daniel.martensson100@outlook.com>
2020-06-05 22:00:41 +0200

    Update SSD1306.h

--- SSD1306 128x32/SSD1306.h
+++ SSD1306 128x32/SSD1306.h
@@ -21,7 +21,6 @@ void SSD1306_stop_scroll();
 void SSD1306_dim(uint8_t dim);
 void SSD1306_draw_line(uint8_t x0, uint8_t x1, uint8_t y0, uint8_t y1);
 void SSD1306_write_text(char* str, uint8_t x, uint8_t y);
-void SSD1306_draw_char(char c, uint8_t x, uint8_t y);
 void SSD1306_write_bitmap(uint8_t x, uint8_t y, uint8_t* bitmap, uint8_t rows, uint8_t columns);
 
 #endif /* SRC_SSD1306_128X32_SSD1306_H_ */
