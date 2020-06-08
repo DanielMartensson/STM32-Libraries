0a951aaf6f82a7f64c681c63878f46db1a722720
Daniel Mårtensson <daniel.martensson100@outlook.com>
2020-06-08 18:46:47 +0200

    Update LCDTools.h

--- ILI9341 Touch LCD/LCDTools/LCDTools.h
+++ ILI9341 Touch LCD/LCDTools/LCDTools.h
@@ -13,12 +13,14 @@
 #include <stdio.h> // For sprintf
 #include <string.h> // For memset
 #include "../LCDCore/LCD_ILI9341.h"
+#include "pictures.h"
+#include "printf.h"
+#include "cmsis_os.h"
 
-void create_main_frame(ILI9341_SPI* spi, bool closedloop_on, bool only_change_loop_icon);
-void create_num_pad_frame(ILI9341_SPI* spi, bool decimalbutton_show, bool minusbutton_show);
-void change_plot_values(ILI9341_SPI *spi, uint16_t new_input, uint16_t new_output, bool only_move_array);
-int16_t num_pad_logic(ILI9341_SPI *spi, bool decimalbutton_show, bool minusbutton_show, float *number_value);
-void show_question_dialog(ILI9341_SPI *spi);
-uint8_t question_dialog_logic(ILI9341_SPI *spi);
+void show_main_frame(ILI9341_SPI* spi, bool closedloop_on, bool only_change_loop_icon, bool stable);
+uint8_t show_num_pad_frame(ILI9341_SPI* spi, bool decimalbutton_show, bool minusbutton_show, float* number_value, char* title);
+void show_plot_frame(ILI9341_SPI *spi, float new_input_float, float new_output_float, uint16_t new_input_uint, uint16_t new_output_uint, bool only_move_array, bool clear_plot);
+uint8_t show_question_save_settings_dialog(ILI9341_SPI *spi);
+
 
 #endif /* SRC_LCD_ILI9341_LCDTOOLS_LCDTOOLS_H_ */
