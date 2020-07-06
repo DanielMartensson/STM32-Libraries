2bb70ef834b91a66b04d01b3f39ad79c649eef3e
Daniel Mårtensson <daniel.martensson100@outlook.com>
2020-07-06 14:33:42 +0200

    Update LCDTools.h

--- ILI9341 Touch LCD/LCDTools/LCDTools.h
+++ ILI9341 Touch LCD/LCDTools/LCDTools.h
@@ -17,7 +17,7 @@
 #include "printf.h"
 #include "cmsis_os.h"
 
-void show_main_frame(ILI9341_SPI* spi, bool closedloop_on, bool only_change_loop_icon, bool stable);
+void show_main_frame(ILI9341_SPI* spi, bool closedloop_on, bool only_change_loop_icon);
 uint8_t show_num_pad_frame(ILI9341_SPI* spi, bool decimalbutton_show, bool minusbutton_show, float* number_value, char* title);
 void show_plot_frame(ILI9341_SPI *spi, float new_input_float, float new_output_float, uint16_t new_input_uint, uint16_t new_output_uint, bool only_move_array, bool clear_plot);
 uint8_t show_question_save_settings_dialog(ILI9341_SPI *spi);
