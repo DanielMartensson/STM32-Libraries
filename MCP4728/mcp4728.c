e2420b96a0a7ad023c333a080e8a12ed60791919
Daniel Mårtensson <daniel.martensson100@outlook.com>
2022-05-05 21:17:50 +0200

    Update mcp4728.c

--- MCP4728/mcp4728.c
+++ MCP4728/mcp4728.c
@@ -28,9 +28,12 @@ void mcp4728_init(I2C_HandleTypeDef *hi2c, uint8_t address){
 }
 
 void mcp4728_single_write_command(uint8_t channel, uint8_t Vref, uint8_t power_down, uint8_t gain, uint16_t value){
+	HAL_GPIO_WritePin(LDAC_GPIO_Port, LDAC_Pin, GPIO_PIN_SET);
+	HAL_Delay(10);
 	const uint8_t UDAC = 0x0;
 	uint8_t pData[3] = {(SINGLE_WRITE_COMMAND << 3) | (channel << 1) | UDAC, (Vref << 8) | (power_down << 5) | (gain << 4) | (value >> 8), value};
 	HAL_I2C_Master_Transmit(_hi2c, _address << 1, pData, sizeof(pData), Timeout);
+	HAL_Delay(10);
 	HAL_GPIO_WritePin(LDAC_GPIO_Port, LDAC_Pin, GPIO_PIN_RESET);
 	HAL_Delay(10);
 	HAL_GPIO_WritePin(LDAC_GPIO_Port, LDAC_Pin, GPIO_PIN_SET);
