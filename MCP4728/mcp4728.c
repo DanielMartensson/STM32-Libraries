a3ca4daf404e2f95b22d64add1b2e00d7c66bf5a
Daniel Mårtensson <daniel.martensson100@outlook.com>
2022-05-07 12:31:28 +0200

    Add files via upload

--- MCP4728/mcp4728.c
+++ MCP4728/mcp4728.c
@@ -8,7 +8,12 @@ static uint8_t _address;
 #define GENERAL_CALL_RESET 0x6
 #define GENERAL_CALL_WAKE_UP 0x9
 #define SINGLE_WRITE_COMMAND 0xB
+#define MULTI_WRITE_COMMAND 0x8
 
+static uint16_t channel_value[4] = {0};
+static uint8_t channel_power_down[4] = {0};
+static uint8_t channel_gain[4] = {0};
+static uint8_t channel_vref[4] = {0};
 
 void static transmit_reset(){
 	uint8_t pData[1] = {GENERAL_CALL_RESET};
@@ -25,16 +30,42 @@ void mcp4728_init(I2C_HandleTypeDef *hi2c, uint8_t address){
 	_address = (0xC << 3) | address;
 	transmit_reset();
 	transmit_wake_up();
+	HAL_GPIO_WritePin(LDAC_GPIO_Port, LDAC_Pin, GPIO_PIN_RESET);
 }
 
-void mcp4728_single_write_command(uint8_t channel, uint8_t Vref, uint8_t power_down, uint8_t gain, uint16_t value){
-	HAL_GPIO_WritePin(LDAC_GPIO_Port, LDAC_Pin, GPIO_PIN_SET);
-	HAL_Delay(10);
+void mcp4728_set_channel_value(uint8_t channel, uint16_t value){
+	if(channel <= MCP4728_CHANNEL_D)
+		channel_value[channel] = value;
+}
+
+void mcp4728_set_channel_power_down(uint8_t channel, uint8_t power_down){
+	if(channel <= MCP4728_CHANNEL_D)
+		channel_power_down[channel] = power_down;
+}
+
+void mcp4728_set_channel_gain(uint8_t channel, uint8_t gain){
+	if(channel <= MCP4728_CHANNEL_D)
+		channel_gain[channel] = gain;
+}
+
+void mcp4728_set_channel_vref(uint8_t channel, uint8_t vref){
+	if(channel <= MCP4728_CHANNEL_D)
+		channel_vref[channel] = vref;
+}
+
+void mcp4728_multi_write_command(){
 	const uint8_t UDAC = 0x0;
-	uint8_t pData[3] = {(SINGLE_WRITE_COMMAND << 3) | (channel << 1) | UDAC, (Vref << 8) | (power_down << 5) | (gain << 4) | (value >> 8), value};
-	HAL_I2C_Master_Transmit(_hi2c, _address << 1, pData, sizeof(pData), Timeout);
-	HAL_Delay(10);
-	HAL_GPIO_WritePin(LDAC_GPIO_Port, LDAC_Pin, GPIO_PIN_RESET);
-	HAL_Delay(10);
-	HAL_GPIO_WritePin(LDAC_GPIO_Port, LDAC_Pin, GPIO_PIN_SET);
+	for(uint8_t channel = MCP4728_CHANNEL_A; channel <= MCP4728_CHANNEL_D; channel++){
+		uint8_t multi_write_data[3] = {(MULTI_WRITE_COMMAND << 3) | (channel << 1) | UDAC, (channel_vref[channel] << 7) | (channel_power_down[channel] << 5) | (channel_gain[channel] << 4) | (channel_value[channel] >> 8), channel_value[channel]};
+		HAL_I2C_Master_Transmit(_hi2c, _address << 1, multi_write_data, sizeof(multi_write_data), Timeout);
+	}
+}
+
+void mcp4728_single_write_command(uint8_t channel){
+	/* This command should only be called with a time delay about 50 ms due to the EEPROM writing */
+	if(channel <= MCP4728_CHANNEL_D){
+		const uint8_t UDAC = 0x0;
+		uint8_t pData[3] = {(SINGLE_WRITE_COMMAND << 3) | (channel << 1) | UDAC, (channel_vref[channel] << 7) | (channel_power_down[channel] << 5) | (channel_gain[channel] << 4) | (channel_value[channel] >> 8), channel_value[channel]};
+		HAL_I2C_Master_Transmit(_hi2c, _address << 1, pData, sizeof(pData), Timeout);
+	}
 }
--- MCP4728/mcp4728.h
+++ MCP4728/mcp4728.h
@@ -1,3 +1,10 @@
+/*
+ * mcp4728.h
+ *
+ *  Created on: May 1, 2022
+ *      Author: Daniel Mårtensson
+ */
+
 #ifndef SRC_MCP4728_MCP4728_H_
 #define SRC_MCP4728_MCP4728_H_
 
@@ -18,6 +25,11 @@
 #define MCP4728_GAIN_2 0x1
 
 void mcp4728_init(I2C_HandleTypeDef *hi2c, uint8_t address);
-void mcp4728_single_write_command(uint8_t channel, uint8_t Vref, uint8_t power_down, uint8_t gain, uint16_t value);
+void mcp4728_set_channel_value(uint8_t channel, uint16_t value);
+void mcp4728_set_channel_power_down(uint8_t channel, uint8_t power_down);
+void mcp4728_set_channel_gain(uint8_t channel, uint8_t gain);
+void mcp4728_set_channel_vref(uint8_t channel, uint8_t vref);
+void mcp4728_multi_write_command();
+void mcp4728_single_write_command(uint8_t channel);
 
 #endif /* SRC_MCP4728_MCP4728_H_ */
